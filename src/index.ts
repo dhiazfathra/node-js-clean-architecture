04859adb5e6ecc728e227d1a40733249911a9154
dhiazfathra <dhiazfathra@gmail.com>
2024-05-18 17:34:11 +0700

    Revert "feat: setup clean architecture"
    This reverts commit bfb8e9564a0934624eac9bd7d933058b3172230d.

--- src/index.ts
+++
@@ -1,2 +0,0 @@
-import { sampleRoutes } from "./adapters/express/routes/SampleRoute";
-import "./adapters/express/server";
--- src/infrastructure/repositories/IRepository.ts
+++
@@ -1,8 +0,0 @@
-import { Document } from "mongoose";
-
-interface IRepository<T extends Document> {
-  add(entity: T): Promise<T>;
-  getAll(): Promise<T[]>;
-}
-
-export default IRepository;
