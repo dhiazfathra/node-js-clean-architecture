0e9061f124462aa37ff9c8154ce56aba0d762d24
dhiazfathra <dhiazfathra@gmail.com>
2024-05-19 12:53:03 +0700

    feat: setup agenda (#18)
    # Description
    
    Setup Agenda to be used as the job scheduler
    
    ## Issues
    
    - Closes #17 
    
    # Preview
    
    <img width="779" alt="Screenshot 2024-05-19 at 12 50 00"
    src="https://github.com/dhiazfathra/node-js-clean-architecture/assets/14912300/eb036eb3-4dd9-4917-8b68-05264672a109">

--- src/adapters/express/server.ts
+++ src/adapters/express/server.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import { sampleRoutes } from "./routes/SampleRoute";
 import { ApiKeyMiddleware } from "./middleware/ApiKeyMiddleware";
 import { userRoutes } from "./routes/UserRoute";
+import Agenda from "agenda";
 
 const app = express();
 const port = 3000;
@@ -16,14 +17,26 @@ app.all("/health", (_req, res) => {
   res.status(200).json({ message: "OK" });
 });
 
+// Uncomment to turn on API key middleware
 // app.use(ApiKeyMiddleware);
 
 // Connect to MongoDB using Mongoose
 let con_string_dev = "mongodb://localhost:27018/nodejsdb";
 let con_string_pro = "mongodb://mongo_db_service:27017/nodejsdb";
-mongoose.connect(
-  process.env.NODE_ENV === "production" ? con_string_pro : con_string_dev
-);
+let mongoConnectionString =
+  process.env.NODE_ENV === "production" ? con_string_pro : con_string_dev;
+mongoose.connect(mongoConnectionString);
+
+// Init Singleton Agenda
+const agenda = new Agenda({
+  db: {
+    address: mongoConnectionString,
+    collection: "agendaJobs",
+  },
+});
+agenda
+  .on("ready", () => console.log("Agenda started!"))
+  .on("error", () => console.log("Agenda connection error!"));
 
 // Routes
 app.use("/api/samples", sampleRoutes);
