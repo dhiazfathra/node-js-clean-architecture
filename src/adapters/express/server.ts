47e1d00f6b3b62ad8305ddaaa59a05e44e929264
dhiazfathra <dhiazfathra@gmail.com>
2024-05-20 01:42:30 +0700

    refactor: add api versioning (#32)

--- src/adapters/express/server.ts
+++ src/adapters/express/server.ts
@@ -2,7 +2,6 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import { sampleRoutes } from "./routes/SampleRoute";
-import { ApiKeyMiddleware } from "./middleware/ApiKeyMiddleware";
 import { userRoutes } from "./routes/UserRoute";
 import { mongoConnectionString } from "../../data/constants/Config";
 const app = express();
@@ -12,20 +11,16 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
-app.all("/health", (_req, res) => {
+app.all("/api/v1/health", (_req, res) => {
   res.status(200).json({ message: "OK" });
 });
-
-// Uncomment to turn on API key middleware
-// app.use(ApiKeyMiddleware);
 
 // Connect to MongoDB using Mongoose
 mongoose.connect(mongoConnectionString);
 
 // Routes
-app.use("/api/samples", sampleRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/user", userRoutes);
+app.use("/api/v1/samples", sampleRoutes);
+app.use("/api/v1/user", userRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
