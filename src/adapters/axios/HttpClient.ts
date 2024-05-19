614e4099c263b9ce13bc2bf99e48f8ace26ead0d
dhiazfathra <dhiazfathra@gmail.com>
2024-05-20 00:10:01 +0700

    fix: fix circular dependency on axios

--- src/adapters/axios/HttpClient.ts
+++ src/adapters/axios/HttpClient.ts
@@ -1,15 +1,5 @@
 import { AxiosResponse } from "axios";
-import { AxiosRetryClient } from "./AxiosRetryClient";
-import { axiosInstance } from "./AxiosConfig";
 
 export interface HttpClient {
   post<T>(url: string, data: any): Promise<AxiosResponse<T>>;
 }
-
-export class AxiosHttpClient implements HttpClient {
-  async post<T>(url: string, data: any): Promise<AxiosResponse<T>> {
-    return axiosInstance.post<T>(url, data);
-  }
-}
-
-export const axiosRetryClient = new AxiosRetryClient();
